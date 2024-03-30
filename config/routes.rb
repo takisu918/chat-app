616ac3bac543bb7e3a3697b4b6ee235ff4ac6bc1
takisu918 <ta.kiss.ryokushakeiba0930@gmail.com>
2024-03-31 04:11:02 +0900

    投稿時刻の日本時間への修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
     resources :messages, only: [:index, :create]
   end
 end
